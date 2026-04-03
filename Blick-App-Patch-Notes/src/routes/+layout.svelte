60db3a649cabfb9c5771d67147bb75c256896f8d
rmdkarhdwn <dkgkrlrnlvksgsk@gmail.com>
2026-04-03 12:12:45 +0900

    chore: set app title and favicon to 블릭패치노트

---
+++ Blick-App-Patch-Notes/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import favicon from '$lib/assets/gride.png';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>블릭패치노트</title>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
